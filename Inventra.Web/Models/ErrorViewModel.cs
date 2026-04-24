4b4a0152bf88b8dddc7997924ec94b69d201ff7d
AigerimAA <aigerim.10803@gmail.com>
2026-04-24 14:00:59 +0500

    Добавьте файлы проекта.

---
+++ Inventra.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inventra.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
