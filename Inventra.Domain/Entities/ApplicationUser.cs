aa3216cd5b2079798ff36cb5e1904d818de70895
AigerimAA <aigerim.10803@gmail.com>
2026-04-24 14:35:02 +0500

    init: add Domain layer structure with entities, enums and interfaces

---
+++ Inventra.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+
+namespace Inventra.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? AvatarUrl { get; set; }
+        public string Language { get; set; } = "en";
+        public string Theme { get; set; } = "light";
+        public bool IsBlocked { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Inventory> OwnedInventories { get; set; } = new List<Inventory>();
+        public ICollection<InventoryAccess> InventoryAccesses { get; set; } = new List<InventoryAccess>();
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+        public ICollection<Like> Likes { get; set; } = new List<Like>();
+    }
+}
---
+++ Inventra.Domain/Enums/FieldType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventra.Domain.Enums
+{
+    public class FieldType
+    {
+    }
+}
