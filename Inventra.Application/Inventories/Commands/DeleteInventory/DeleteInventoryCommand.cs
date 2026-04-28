881da6fb2ae1cef92605a83d1117a7c7ff013602
AigerimAA <aigerim.10803@gmail.com>
2026-04-28 17:00:09 +0500

    feat: add DeleteInventory command with handler

--- Inventra.Application/Inventories/Commands/DeleteInventory/DeleteInventoryCommand.cs
+++ Inventra.Application/Inventories/Commands/DeleteInventory/DeleteInventoryCommand.cs
@@ -1,6 +1,7 @@
-﻿namespace Inventra.Application.Inventories.Commands.DeleteInventory
+﻿using MediatR;
+
+namespace Inventra.Application.Inventories.Commands.DeleteInventory
 {
-    public class DeleteInventoryCommand
-    {
-    }
+    public record DeleteInventoryCommand(int Id) : IRequest;
+    
 }
--- Inventra.Application/Inventories/Commands/DeleteInventory/DeleteInventoryCommandHandler.cs
+++ Inventra.Application/Inventories/Commands/DeleteInventory/DeleteInventoryCommandHandler.cs
@@ -1,6 +1,29 @@
-﻿namespace Inventra.Application.Inventories.Commands.DeleteInventory
+﻿using Inventra.Application.Common.Exceptions;
+using Inventra.Application.Interfaces;
+using Inventra.Domain.Entities;
+using Inventra.Domain.Interfaces;
+using MediatR;
+
+namespace Inventra.Application.Inventories.Commands.DeleteInventory
 {
-    public class DeleteInventoryCommandHandler
+    public class DeleteInventoryCommandHandler : IRequestHandler<DeleteInventoryCommand>
     {
+        private readonly IInventoryRepository _inventoryRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteInventoryCommandHandler(IInventoryRepository inventoryRepository, IUnitOfWork unitOfWork)
+        {
+            _inventoryRepository = inventoryRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(DeleteInventoryCommand request, CancellationToken cancellationToken)
+        {
+            var inventory = await _inventoryRepository.GetByIdAsync(request.Id)
+                    ?? throw new NotFoundException(nameof(Inventory), request.Id);
+
+            await _inventoryRepository.DeleteAsync(inventory.Id);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
     }
 }
