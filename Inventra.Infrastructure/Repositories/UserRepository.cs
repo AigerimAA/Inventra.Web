ba774ae7bd396beb0a49328a3e74fc1c0f9051f9
AigerimAA <aigerim.10803@gmail.com>
2026-04-28 21:47:02 +0500

    feat: add UserRepository implementation

--- Inventra.Infrastructure/Repositories/UserRepository.cs
+++ Inventra.Infrastructure/Repositories/UserRepository.cs
@@ -1,6 +1,37 @@
-﻿namespace Inventra.Infrastructure.Repositories
+﻿using Inventra.Domain.Entities;
+using Inventra.Domain.Interfaces;
+using Inventra.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventra.Infrastructure.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ApplicationUser?> GetByIdAsync(string id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+        public async Task<ApplicationUser?> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+        public async Task<IEnumerable<ApplicationUser>> SearchByNameOrEmailAsync(string query)
+        {
+            return await _context.Users
+                .Where(u => u.UserName!.Contains(query) || u.Email!.Contains(query))
+                .Take(10)
+                .ToListAsync();
+        }
+        public async Task UpdateAsync(ApplicationUser user)
+        {
+            _context.Users.Update(user);
+        }
     }
 }
