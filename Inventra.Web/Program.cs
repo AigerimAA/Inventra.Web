c9c952ace9696e05dfe452d1f61efcb648af4f4a
AigerimAA <aigerim.10803@gmail.com>
2026-04-29 11:47:52 +0500

    feat: configure Program.cs with all services and middleware

--- Inventra.Web/Program.cs
+++ Inventra.Web/Program.cs
@@ -1,3 +1,16 @@
+using FluentValidation;
+using Inventra.Application.Common.Mappings;
+using Inventra.Application.Interfaces;
+using Inventra.Application.Inventories.Commands.CreateInventory;
+using Inventra.Domain.Interfaces;
+using Inventra.Infrastructure.Persistence;
+using Inventra.Infrastructure.Repositories;
+using Inventra.Infrastructure.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using CloudinaryDotNet;
+using Inventra.Domain.Entities;
+
 namespace Inventra.Web
 {
     public class Program
@@ -6,29 +19,83 @@ namespace Inventra.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireUppercase = true;
+            })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+            builder.Services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.ClientId = builder.Configuration["Authentication:Google:ClientId"]!;
+                    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]!;
+                })
+                .AddFacebook(options =>
+                {
+                    options.AppId = builder.Configuration["Authentication:Facebook:AppId"]!;
+                    options.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"]!;
+                });
+
+            builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+            builder.Services.AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssembly(
+                        typeof(CreateInventoryCommand).Assembly));
+
+            builder.Services.AddValidatorsFromAssembly(
+                typeof(CreateInventoryCommand).Assembly);
+
+            builder.Services.AddScoped<IInventoryRepository, InventoryRepository>();
+            builder.Services.AddScoped<IItemRepository, ItemRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            var cloudinaryAccount = new Account(
+                builder.Configuration["Cloudinary:CloudName"],
+                builder.Configuration["Cloudinary:ApiKey"],
+                builder.Configuration["Cloudinary:ApiSecret"]);
+            builder.Services.AddSingleton(new Cloudinary(cloudinaryAccount));
+            builder.Services.AddScoped<CloudStorageServices>();
+
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+            builder.Services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[] { "en", "ru", "kk" };
+                options.SetDefaultCulture("en")
+                    .AddSupportedCultures(supportedCultures)
+                    .AddSupportedUICultures(supportedCultures);
+            });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
 
+            app.UseRequestLocalization();
+            app.UseAuthentication();
             app.UseAuthorization();
 
-            app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
