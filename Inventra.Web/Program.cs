078c85fb042a284328de857ee7091c7c0c316c67
AigerimAA <aigerim.10803@gmail.com>
2026-04-29 16:00:50 +0500

    feat: add automatic database migration on startup

--- Inventra.Web/Program.cs
+++ Inventra.Web/Program.cs
@@ -106,6 +106,12 @@ namespace Inventra.Web
 
             var app = builder.Build();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
+            }
+
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
