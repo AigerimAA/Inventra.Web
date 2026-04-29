2da30fa5f9d867a4fd6cc783eca177d534dcd205
AigerimAA <aigerim.10803@gmail.com>
2026-04-29 20:51:12 +0500

    feat: add inventory edit with settings and custom fields management

--- Inventra.Web/Controllers/InventoryController.cs
+++ Inventra.Web/Controllers/InventoryController.cs
@@ -1,6 +1,8 @@
 ﻿using Inventra.Application.Categories.Queries.GetAllCategories;
+using Inventra.Application.DTOs;
 using Inventra.Application.Inventories.Commands.CreateInventory;
 using Inventra.Application.Inventories.Commands.DeleteInventory;
+using Inventra.Application.Inventories.Commands.UpdateInventory;
 using Inventra.Application.Inventories.Queries.GetAllInventories;
 using Inventra.Application.Inventories.Queries.GetInventoryById;
 using Inventra.Domain.Entities;
@@ -78,8 +80,77 @@ namespace Inventra.Web.Controllers
             if (inventory.OwnerId != userId && !User.IsInRole("Admin"))
                 return Forbid();
 
+            var categories = await _mediator.Send(new GetAllCategoriesQuery());
+            ViewBag.Categories = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(categories, "Id", "Name");
+
             return View(inventory);
         }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditSettings(InventoryDto dto)
+        {
+            var userId = _userManager.GetUserId(User);
+            if (dto.OwnerId != userId && !User.IsInRole("Admin"))
+                return Forbid();
+
+            var command = new UpdateInventoryCommand
+            {
+                Id = dto.Id,
+                Title = dto.Title,
+                Description = dto.Description,
+                IsPublic = dto.IsPublic,
+                CategoryId = dto.CategoryId,
+                Version = dto.Version
+            };
+
+            try
+            {
+                await _mediator.Send(command);
+            }
+            catch(Inventra.Application.Common.Exceptions.ConcurrencyException)
+            {
+                ModelState.AddModelError(string.Empty, "Someone else modified this inventory. Please reload and try again");
+                return View("Edit", dto);
+            }
+            return RedirectToAction(nameof(Details), new { id = dto.Id });
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditFields(InventoryDto dto)
+        {
+            var userId = _userManager.GetUserId(User);
+            if (dto.OwnerId != userId && !User.IsInRole("Admin"))
+                return Forbid();
+
+            var command = new UpdateInventoryCommand
+            {
+                Id = dto.Id,
+                Title = dto.Title ?? string.Empty,
+                Version = dto.Version,
+                CustomString1Name = dto.CustomString1Name,
+                CustomString2Name = dto.CustomString2Name,
+                CustomString3Name = dto.CustomString3Name,
+                CustomInt1Name = dto.CustomInt1Name,
+                CustomInt2Name = dto.CustomInt2Name,
+                CustomInt3Name = dto.CustomInt3Name,
+                CustomText1Name = dto.CustomText1Name,
+                CustomText2Name = dto.CustomText2Name,
+                CustomText3Name = dto.CustomText3Name,
+                CustomBool1Name = dto.CustomBool1Name,
+                CustomBool2Name = dto.CustomBool2Name,
+                CustomBool3Name = dto.CustomBool3Name,
+                CustomLink1Name = dto.CustomLink1Name,
+                CustomLink2Name = dto.CustomLink2Name,
+                CustomLink3Name = dto.CustomLink3Name
+            };
+            await _mediator.Send(command);
+            return RedirectToAction(nameof(Details), new {id = dto.Id});
+        }
+
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
