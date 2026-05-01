b7e92223fa04e8e792bfa8a99d043ba32522238b
AigerimAA <aigerim.10803@gmail.com>
2026-05-01 12:02:26 +0500

    feat: add Profile page with owned and accessible inventories

---
+++ Inventra.Web/Controllers/ProfileController.cs
@@ -0,0 +1,41 @@
+﻿using Inventra.Application.Inventories.Queries.GetAllInventories;
+using Inventra.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventra.Web.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfileController(IMediator mediator, UserManager<ApplicationUser> userManager)
+        {
+            _mediator = mediator;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = _userManager.GetUserId(User)!;
+            var allInventories = await _mediator.Send(new GetAllInventoriesQuery());
+
+            var ownedInventories = allInventories
+                .Where(i => string.Equals(i.OwnerId, userId))
+                .ToList();
+
+            var accessibleInventories = allInventories
+                .Where(i => !string.Equals(i.OwnerId, userId))
+                .ToList();
+
+            ViewBag.OwnedInventories = ownedInventories;
+            ViewBag.AccessibleInventories = accessibleInventories;
+
+            return View();
+        }
+    }
+}
---
+++ Inventra.Web/Views/Profile/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Inventra.Web.Views.Profile
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
