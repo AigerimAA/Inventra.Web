3beee647704f3ad6027383f7229b5a080897daa5
AigerimAA <aigerim.10803@gmail.com>
2026-05-03 14:07:13 +0500

    feat: add Full-Text Search indexes for Inventories and Items

---
+++ Inventra.Infrastructure/Migrations/20260503090138_AddFullTextSearch.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventra.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFullTextSearch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE FULLTEXT CATALOG InventraFtsCatalog AS DEFAULT;");
+
+            migrationBuilder.Sql(@"
+                CREATE FULLTEXT INDEX ON Inventories(Title, Description) 
+                KEY INDEX PK_Inventories 
+                ON InventraFtsCatalog 
+                WITH (CHANGE_TRACKING = AUTO);");
+
+            migrationBuilder.Sql(@"
+                CREATE FULLTEXT INDEX ON Items(CustomId, CustomString1Value, CustomString2Value, 
+                    CustomString3Value, CustomText1Value, CustomText2Value, CustomText3Value) 
+                KEY INDEX PK_Items 
+                ON InventraFtsCatalog 
+                WITH (CHANGE_TRACKING = AUTO);");
+                }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FULLTEXT INDEX ON Items;");
+            migrationBuilder.Sql("DROP FULLTEXT INDEX ON Inventories;");
+            migrationBuilder.Sql("DROP FULLTEXT CATALOG InventraFtsCatalog;");
+        }
+    }
+}
