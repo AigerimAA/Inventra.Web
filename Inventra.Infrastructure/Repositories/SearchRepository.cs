04340b62ea7cd18e99e6ea5806d2c7ce6280d870
AigerimAA <aigerim.10803@gmail.com>
2026-05-03 14:10:39 +0500

    feat: implement Full-Text Search in SearchRepository

--- Inventra.Infrastructure/Repositories/SearchRepository.cs
+++ Inventra.Infrastructure/Repositories/SearchRepository.cs
@@ -16,24 +16,41 @@ namespace Inventra.Infrastructure.Repositories
 
         public async Task<IEnumerable<Inventory>> SearchInventoriesAsync(string query)
         {
+            if (string.IsNullOrWhiteSpace(query))
+                return Enumerable.Empty<Inventory>();
+
             return await _context.Inventories
                 .Include(i => i.Owner)
                 .Include(i => i.Category)
                 .Include(i => i.Items)
-                .Where(i => i.Title.Contains(query) ||
-                        (i.Description != null && i.Description.Contains(query)))
+                .Where(i => EF.Functions.FreeText(i.Title, query)
+                         || (i.Description != null
+                             && EF.Functions.FreeText(i.Description, query)))
                 .ToListAsync();
         }
         public async Task<IEnumerable<Item>> SearchItemsAsync(string query)
         {
+            if (string.IsNullOrWhiteSpace(query))
+                return Enumerable.Empty<Item>();
+
             return await _context.Items
                 .Include(i => i.Inventory)
                 .Include(i => i.CreatedBy)
-                .Where(i => i.CustomId.Contains(query) || 
-                        (i.CustomString1Value != null && i.CustomString1Value.Contains(query)) ||
-                        (i.CustomString2Value != null && i.CustomString2Value.Contains(query)) ||
-                        (i.CustomString3Value != null && i.CustomString3Value.Contains(query)))
-            .ToListAsync();
+                .Where(i =>
+                    EF.Functions.FreeText(i.CustomId, query) ||
+                    (i.CustomString1Value != null &&
+                        EF.Functions.FreeText(i.CustomString1Value, query)) ||
+                    (i.CustomString2Value != null &&
+                        EF.Functions.FreeText(i.CustomString2Value, query)) ||
+                    (i.CustomString3Value != null &&
+                        EF.Functions.FreeText(i.CustomString3Value, query)) ||
+                    (i.CustomText1Value != null &&
+                        EF.Functions.FreeText(i.CustomText1Value, query)) ||
+                    (i.CustomText2Value != null &&
+                        EF.Functions.FreeText(i.CustomText2Value, query)) ||
+                    (i.CustomText3Value != null &&
+                        EF.Functions.FreeText(i.CustomText3Value, query)))
+                .ToListAsync();
         }
     }
 }
